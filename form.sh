878ce47d76b4ab3e0ac02593025b04b209ae06bb
root <root@rmn.local.host>
2021-06-22 11:37:07 +0400

    form

---
+++ form.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "What is your name ?"
+read Name
+echo "Yourname $Name"
+echo "What is your surname ?"
+read Surname 
+echo "Surname $Surname"
+echo "How old are you?"
+read Age
+echo "Age $Age"
+
